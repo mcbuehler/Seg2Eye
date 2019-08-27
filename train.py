7ca016ccc01836a5939ac55affe54438a31f4612
Marcel <buehlmar@ethz.ch>
2019-08-27 13:50:33 +0200

    exception handling and model saving in case of interrupt. Visualisations

--- train.py
+++ train.py
@@ -4,12 +4,24 @@ Licensed under the CC BY-NC-SA 4.0 license (https://creativecommons.org/licenses
 """
 
 import sys
+import traceback
 from collections import OrderedDict
+
+import torch
+
+from data.openeds_dataset import OpenEDSDataset
 from options.train_options import TrainOptions
+import util.validation as validation
 import data
+from util.image_annotate import get_text_image
 from util.iter_counter import IterationCounter
 from util.visualizer import Visualizer
 from trainers.pix2pix_trainer import Pix2PixTrainer
+from tensorboardX.writer import SummaryWriter
+import os
+
+os.environ['CUDA_LAUNCH_BLOCKING'] = "1"
+
 
 # parse options
 opt = TrainOptions().parse()
@@ -20,6 +32,9 @@ print(' '.join(sys.argv))
 # load the dataset
 dataloader = data.create_dataloader(opt)
 
+# Validation
+dataloader_val = data.create_inference_dataloader(opt)
+
 # create trainer for our model
 trainer = Pix2PixTrainer(opt)
 
@@ -28,47 +43,73 @@ iter_counter = IterationCounter(opt, len(dataloader))
 
 # create tool for visualization
 visualizer = Visualizer(opt)
+summary_directory = os.path.join(opt.checkpoints_dir, opt.name, "tensorboard")
+writer = SummaryWriter(summary_directory)
 
-for epoch in iter_counter.training_epochs():
-    iter_counter.record_epoch_start(epoch)
-    for i, data_i in enumerate(dataloader, start=iter_counter.epoch_iter):
-        iter_counter.record_one_iteration()
+# opt.display_freq = 1
+# opt.print_freq = 1
+# opt.validation_limit = 5
 
-        # Training
-        # train generator
-        if i % opt.D_steps_per_G == 0:
-            trainer.run_generator_one_step(data_i)
+try:
+    for epoch in iter_counter.training_epochs():
+        if iter_counter.current_epoch != epoch:
+            # They are only equal at the very beginning and after loading a model
+            iter_counter.record_epoch_start(epoch)
 
-        # train discriminator
-        trainer.run_discriminator_one_step(data_i)
+        for i, data_i in enumerate(dataloader, start=iter_counter.epoch_iter):
+            iter_counter.record_one_iteration()
 
-        # Visualizations
-        if iter_counter.needs_printing():
-            losses = trainer.get_latest_losses()
-            visualizer.print_current_errors(epoch, iter_counter.epoch_iter,
-                                            losses, iter_counter.time_per_iter)
-            visualizer.plot_current_errors(losses, iter_counter.total_steps_so_far)
+            # Training
+            # train generator
+            if i % opt.D_steps_per_G == 0:
+                trainer.run_generator_one_step(data_i)
 
-        if iter_counter.needs_displaying():
-            visuals = OrderedDict([('input_label', data_i['label']),
-                                   ('synthesized_image', trainer.get_latest_generated()),
-                                   ('real_image', data_i['image'])])
-            visualizer.display_current_results(visuals, epoch, iter_counter.total_steps_so_far)
+            # train discriminator
+            trainer.run_discriminator_one_step(data_i)
 
-        if iter_counter.needs_saving():
-            print('saving the latest model (epoch %d, total_steps %d)' %
+            # Visualizations
+            if iter_counter.needs_printing():
+                losses = trainer.get_latest_losses()
+                visualizer.print_current_errors(epoch, iter_counter.epoch_iter,
+                                                losses, iter_counter.time_per_iter)
+                visualizer.plot_current_errors(losses, iter_counter.total_steps_so_far)
+
+            if iter_counter.needs_displaying():
+                visuals = OrderedDict([('input_label', data_i['label']),
+                                       ('synthesized_image', trainer.get_latest_generated()),
+                                       ('real_image', data_i['image'])])
+                visualizer.display_current_results(visuals, epoch, iter_counter.total_steps_so_far)
+
+                # Output VALIDATION images
+                validation.run_validation(dataloader_val, trainer.pix2pix_model, visualizer, epoch, iter_counter, limit=opt.validation_limit)
+
+            if iter_counter.needs_saving():
+                print('saving the latest model (epoch %d, total_steps %d)' %
+                      (epoch, iter_counter.total_steps_so_far))
+                trainer.save('latest')
+                iter_counter.record_current_iter()
+
+        trainer.update_learning_rate(epoch)
+        iter_counter.record_epoch_end()
+
+        if epoch % opt.save_epoch_freq == 0 or \
+           epoch == iter_counter.total_epochs:
+            print('saving the model at the end of epoch %d, iters %d' %
                   (epoch, iter_counter.total_steps_so_far))
             trainer.save('latest')
-            iter_counter.record_current_iter()
+            trainer.save(epoch)
+    print('Training was successfully finished.')
 
-    trainer.update_learning_rate(epoch)
-    iter_counter.record_epoch_end()
+except (KeyboardInterrupt, SystemExit):
+        print("KeyboardInterrupt. Shutting down.")
+        print(traceback.format_exc())
+except Exception as e:
+    print(traceback.format_exc())
+finally:
+    print('saving the model before quitting')
+    trainer.save('latest')
+    iter_counter.record_current_iter()
+    del dataloader_val
+    del dataloader
 
-    if epoch % opt.save_epoch_freq == 0 or \
-       epoch == iter_counter.total_epochs:
-        print('saving the model at the end of epoch %d, iters %d' %
-              (epoch, iter_counter.total_steps_so_far))
-        trainer.save('latest')
-        trainer.save(epoch)
 
-print('Training was successfully finished.')
