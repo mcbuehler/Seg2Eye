f2bf876806c8553b81e4c0d4d2ae15c9d05b115b
Marcel <buehlmar@ethz.ch>
2019-08-27 13:40:26 +0200

    variable number of output channels. Removed unused pix2pix generator

--- models/networks/generator.py
+++ models/networks/generator.py
@@ -53,7 +53,7 @@ class SPADEGenerator(BaseNetwork):
             self.up_4 = SPADEResnetBlock(1 * nf, nf // 2, opt)
             final_nc = nf // 2
 
-        self.conv_img = nn.Conv2d(final_nc, 3, 3, padding=1)
+        self.conv_img = nn.Conv2d(final_nc, opt.output_nc, 3, padding=1)
 
         self.up = nn.Upsample(scale_factor=2)
 
@@ -116,67 +116,3 @@ class SPADEGenerator(BaseNetwork):
         x = F.tanh(x)
 
         return x
-
-
-class Pix2PixHDGenerator(BaseNetwork):
-    @staticmethod
-    def modify_commandline_options(parser, is_train):
-        parser.add_argument('--resnet_n_downsample', type=int, default=4, help='number of downsampling layers in netG')
-        parser.add_argument('--resnet_n_blocks', type=int, default=9, help='number of residual blocks in the global generator network')
-        parser.add_argument('--resnet_kernel_size', type=int, default=3,
-                            help='kernel size of the resnet block')
-        parser.add_argument('--resnet_initial_kernel_size', type=int, default=7,
-                            help='kernel size of the first convolution')
-        parser.set_defaults(norm_G='instance')
-        return parser
-
-    def __init__(self, opt):
-        super().__init__()
-        input_nc = opt.label_nc + (1 if opt.contain_dontcare_label else 0) + (0 if opt.no_instance else 1)
-
-        norm_layer = get_nonspade_norm_layer(opt, opt.norm_G)
-        activation = nn.ReLU(False)
-
-        model = []
-
-        # initial conv
-        model += [nn.ReflectionPad2d(opt.resnet_initial_kernel_size // 2),
-                  norm_layer(nn.Conv2d(input_nc, opt.ngf,
-                                       kernel_size=opt.resnet_initial_kernel_size,
-                                       padding=0)),
-                  activation]
-
-        # downsample
-        mult = 1
-        for i in range(opt.resnet_n_downsample):
-            model += [norm_layer(nn.Conv2d(opt.ngf * mult, opt.ngf * mult * 2,
-                                           kernel_size=3, stride=2, padding=1)),
-                      activation]
-            mult *= 2
-
-        # resnet blocks
-        for i in range(opt.resnet_n_blocks):
-            model += [ResnetBlock(opt.ngf * mult,
-                                  norm_layer=norm_layer,
-                                  activation=activation,
-                                  kernel_size=opt.resnet_kernel_size)]
-
-        # upsample
-        for i in range(opt.resnet_n_downsample):
-            nc_in = int(opt.ngf * mult)
-            nc_out = int((opt.ngf * mult) / 2)
-            model += [norm_layer(nn.ConvTranspose2d(nc_in, nc_out,
-                                                    kernel_size=3, stride=2,
-                                                    padding=1, output_padding=1)),
-                      activation]
-            mult = mult // 2
-
-        # final output conv
-        model += [nn.ReflectionPad2d(3),
-                  nn.Conv2d(nc_out, opt.output_nc, kernel_size=7, padding=0),
-                  nn.Tanh()]
-
-        self.model = nn.Sequential(*model)
-
-    def forward(self, input, z=None):
-        return self.model(input)
