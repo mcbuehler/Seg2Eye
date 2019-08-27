7b1b40ace445b7b9e1f641655a29c29cbd9d50be
Marcel <buehlmar@ethz.ch>
2019-08-27 13:45:47 +0200

    updating default options and adding a few new ones

--- options/test_options.py
+++ options/test_options.py
@@ -9,7 +9,7 @@ from .base_options import BaseOptions
 class TestOptions(BaseOptions):
     def initialize(self, parser):
         BaseOptions.initialize(self, parser)
-        parser.add_argument('--results_dir', type=str, default='./results/', help='saves results here.')
+        parser.add_argument('--results_dir', type=str, default='results/', help='saves results here.')
         parser.add_argument('--which_epoch', type=str, default='latest', help='which epoch to load? set to latest to use latest cached model')
         parser.add_argument('--how_many', type=int, default=float("inf"), help='how many test images to run')
 
@@ -17,5 +17,6 @@ class TestOptions(BaseOptions):
         parser.set_defaults(serial_batches=True)
         parser.set_defaults(no_flip=True)
         parser.set_defaults(phase='test')
+        parser.set_defaults(keep_original=True)
         self.isTrain = False
         return parser
--- options/train_options.py
+++ options/train_options.py
@@ -10,13 +10,14 @@ class TrainOptions(BaseOptions):
     def initialize(self, parser):
         BaseOptions.initialize(self, parser)
         # for displays
-        parser.add_argument('--display_freq', type=int, default=100, help='frequency of showing training results on screen')
-        parser.add_argument('--print_freq', type=int, default=100, help='frequency of showing training results on console')
+        parser.add_argument('--display_freq', type=int, default=1000, help='frequency of showing training results on screen')
+        parser.add_argument('--print_freq', type=int, default=500, help='frequency of showing training results on console')
         parser.add_argument('--save_latest_freq', type=int, default=5000, help='frequency of saving the latest results')
         parser.add_argument('--save_epoch_freq', type=int, default=10, help='frequency of saving checkpoints at the end of epochs')
         parser.add_argument('--no_html', action='store_true', help='do not save intermediate training results to [opt.checkpoints_dir]/[opt.name]/web/')
         parser.add_argument('--debug', action='store_true', help='only do one epoch and displays at each iteration')
         parser.add_argument('--tf_log', action='store_true', help='if specified, use tensorboard logging. Requires tensorflow installed')
+        parser.add_argument('--validation_limit', type=int, default=500, help='Number of samples to use for calculating mse')
 
         # for training
         parser.add_argument('--continue_train', action='store_true', help='continue training: load the latest model')
