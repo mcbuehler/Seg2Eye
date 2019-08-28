7e1f656903868008ae4e492d15ed2dd137f6bf5e
Marcel <buehlmar@ethz.ch>
2019-08-28 17:13:21 +0200

    error log as parser argument

--- options/test_options.py
+++ options/test_options.py
@@ -12,11 +12,13 @@ class TestOptions(BaseOptions):
         parser.add_argument('--results_dir', type=str, default='results/', help='saves results here.')
         parser.add_argument('--which_epoch', type=str, default='latest', help='which epoch to load? set to latest to use latest cached model')
         parser.add_argument('--how_many', type=int, default=float("inf"), help='how many test images to run')
+        parser.add_argument('--write_error_log', action='store_true', help='only for validation. write errors to file')
 
         parser.set_defaults(preprocess_mode='scale_width_and_crop', crop_size=256, load_size=256, display_winsize=256)
         parser.set_defaults(serial_batches=True)
         parser.set_defaults(no_flip=True)
         parser.set_defaults(phase='test')
         parser.set_defaults(keep_original=True)
+        parser.set_defaults(write_error_log=False)
         self.isTrain = False
         return parser
