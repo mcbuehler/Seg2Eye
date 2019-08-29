278b97c3afc861c9af9ddf4ed9977704bd366db6
Marcel <buehlmar@ethz.ch>
2019-08-29 09:43:58 +0200

    added lambda for l2 loss

--- options/train_options.py
+++ options/train_options.py
@@ -34,6 +34,7 @@ class TrainOptions(BaseOptions):
         parser.add_argument('--ndf', type=int, default=64, help='# of discrim filters in first conv layer')
         parser.add_argument('--lambda_feat', type=float, default=10.0, help='weight for feature matching loss')
         parser.add_argument('--lambda_vgg', type=float, default=10.0, help='weight for vgg loss')
+        parser.add_argument('--lambda_l2', type=float, default=5.0, help='weight for L2 loss')
         parser.add_argument('--no_ganFeat_loss', action='store_true', help='if specified, do *not* use discriminator feature matching loss')
         parser.add_argument('--no_vgg_loss', action='store_true', help='if specified, do *not* use VGG feature matching loss')
         parser.add_argument('--gan_mode', type=str, default='hinge', help='(ls|original|hinge)')
