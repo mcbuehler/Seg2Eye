2002e45dc7f5f239b26de71edc6102868c4215ea
Marcel <buehlmar@ethz.ch>
2019-08-29 12:12:39 +0200

    new generator that uses the spadestyle blocks

--- models/networks/generator.py
+++ models/networks/generator.py
@@ -7,9 +7,7 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 from models.networks.base_network import BaseNetwork
-from models.networks.normalization import get_nonspade_norm_layer
-from models.networks.architecture import ResnetBlock as ResnetBlock
-from models.networks.architecture import SPADEResnetBlock as SPADEResnetBlock
+from models.networks.architecture import SPADEResnetBlock as SPADEResnetBlock, SPADE_STYLE_ResnetBlock
 
 
 class SPADEGenerator(BaseNetwork):
@@ -21,6 +19,9 @@ class SPADEGenerator(BaseNetwork):
                             help="If 'more', adds upsampling layer between the two middle resnet blocks. If 'most', also add one more upsampling + resnet layer at the end of the generator")
 
         return parser
+
+    def _get_resnet_block(self, fin, fout, opt):
+            return SPADEResnetBlock(fin, fout, opt)
 
     def __init__(self, opt):
         super().__init__()
@@ -37,20 +38,20 @@ class SPADEGenerator(BaseNetwork):
             # downsampled segmentation map instead of random z
             self.fc = nn.Conv2d(self.opt.semantic_nc, 16 * nf, 3, padding=1)
 
-        self.head_0 = SPADEResnetBlock(16 * nf, 16 * nf, opt)
+        self.head_0 = self._get_resnet_block(16 * nf, 16 * nf, opt)
 
-        self.G_middle_0 = SPADEResnetBlock(16 * nf, 16 * nf, opt)
-        self.G_middle_1 = SPADEResnetBlock(16 * nf, 16 * nf, opt)
+        self.G_middle_0 = self._get_resnet_block(16 * nf, 16 * nf, opt)
+        self.G_middle_1 = self._get_resnet_block(16 * nf, 16 * nf, opt)
 
-        self.up_0 = SPADEResnetBlock(16 * nf, 8 * nf, opt)
-        self.up_1 = SPADEResnetBlock(8 * nf, 4 * nf, opt)
-        self.up_2 = SPADEResnetBlock(4 * nf, 2 * nf, opt)
-        self.up_3 = SPADEResnetBlock(2 * nf, 1 * nf, opt)
+        self.up_0 = self._get_resnet_block(16 * nf, 8 * nf, opt)
+        self.up_1 = self._get_resnet_block(8 * nf, 4 * nf, opt)
+        self.up_2 = self._get_resnet_block(4 * nf, 2 * nf, opt)
+        self.up_3 = self._get_resnet_block(2 * nf, 1 * nf, opt)
 
         final_nc = nf
 
         if opt.num_upsampling_layers == 'most':
-            self.up_4 = SPADEResnetBlock(1 * nf, nf // 2, opt)
+            self.up_4 = self._get_resnet_block(1 * nf, nf // 2, opt)
             final_nc = nf // 2
 
         self.conv_img = nn.Conv2d(final_nc, opt.output_nc, 3, padding=1)
@@ -111,6 +112,107 @@ class SPADEGenerator(BaseNetwork):
         if self.opt.num_upsampling_layers == 'most':
             x = self.up(x)
             x = self.up_4(x, seg)
+
+        x = self.conv_img(F.leaky_relu(x, 2e-1))
+        x = F.tanh(x)
+
+        return x
+
+
+class SPADESTYLEGenerator(BaseNetwork):
+    @staticmethod
+    def modify_commandline_options(parser, is_train):
+        parser.set_defaults(norm_G='spectralspadesyncbatch3x3')
+        parser.add_argument('--num_upsampling_layers',
+                            choices=('normal', 'more', 'most'), default='normal',
+                            help="If 'more', adds upsampling layer between the two middle resnet blocks. If 'most', also add one more upsampling + resnet layer at the end of the generator")
+
+        return parser
+
+    def _get_resnet_block(self, fin, fout, opt):
+        # DIFF to SPADE. SPADE uses only SPADE Resnet blocks
+        return SPADE_STYLE_ResnetBlock(fin, fout, opt)
+
+    def __init__(self, opt):
+        super().__init__()
+        self.opt = opt
+        nf = opt.ngf
+
+        self.sw, self.sh = self.compute_latent_vector_size(opt)
+
+        # We make the network deterministic by starting with
+        # downsampled segmentation map instead of random z
+        # DIFF to SPADE. They have the option to start from an encoded image
+        self.fc = nn.Conv2d(self.opt.semantic_nc, 16 * nf, 3, padding=1)
+
+        self.head_0 = self._get_resnet_block(16 * nf, 16 * nf, opt)
+
+        self.G_middle_0 = self._get_resnet_block(16 * nf, 16 * nf, opt)
+        self.G_middle_1 = self._get_resnet_block(16 * nf, 16 * nf, opt)
+
+        self.up_0 = self._get_resnet_block(16 * nf, 8 * nf, opt)
+        self.up_1 = self._get_resnet_block(8 * nf, 4 * nf, opt)
+        self.up_2 = self._get_resnet_block(4 * nf, 2 * nf, opt)
+        self.up_3 = self._get_resnet_block(2 * nf, 1 * nf, opt)
+
+        final_nc = nf
+
+        if opt.num_upsampling_layers == 'most':
+            self.up_4 = self._get_resnet_block(1 * nf, nf // 2, opt)
+            final_nc = nf // 2
+
+        self.conv_img = nn.Conv2d(final_nc, opt.output_nc, 3, padding=1)
+
+        self.up = nn.Upsample(scale_factor=2)
+
+    def compute_latent_vector_size(self, opt):
+        # Size for the starting feature map
+        if opt.num_upsampling_layers == 'normal':
+            num_up_layers = 5
+        elif opt.num_upsampling_layers == 'more':
+            num_up_layers = 6
+        elif opt.num_upsampling_layers == 'most':
+            num_up_layers = 7
+        else:
+            raise ValueError('opt.num_upsampling_layers [%s] not recognized' %
+                             opt.num_upsampling_layers)
+
+        sw = opt.crop_size // (2**num_up_layers)
+        sh = round(sw / opt.aspect_ratio)
+
+        return sw, sh
+
+    def forward(self, input, w=None):
+        seg = input
+
+        # we downsample segmap and run convolution
+        # DIFF
+        x = F.interpolate(seg, size=(self.sh, self.sw))
+        x = self.fc(x)
+
+        x = self.head_0(x, seg, w)
+
+        x = self.up(x)
+        x = self.G_middle_0(x, seg, w)
+
+        if self.opt.num_upsampling_layers == 'more' or \
+           self.opt.num_upsampling_layers == 'most':
+            x = self.up(x)
+
+        x = self.G_middle_1(x, seg, w)
+
+        x = self.up(x)
+        x = self.up_0(x, seg, w)
+        x = self.up(x)
+        x = self.up_1(x, seg, w)
+        x = self.up(x)
+        x = self.up_2(x, seg, w)
+        x = self.up(x)
+        x = self.up_3(x, seg, w)
+
+        if self.opt.num_upsampling_layers == 'most':
+            x = self.up(x)
+            x = self.up_4(x, seg, w)
 
         x = self.conv_img(F.leaky_relu(x, 2e-1))
         x = F.tanh(x)
