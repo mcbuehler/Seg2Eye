eef9b6d131b3acdfc23269eea0de56fb8681a52f
Marcel <buehlmar@ethz.ch>
2019-09-30 17:12:07 +0200

    adding Gram matrix consistency loss

--- models/networks/loss.py
+++ models/networks/loss.py
@@ -200,3 +200,30 @@ class MSECalculator:
             # f'mse/{dataset_key}/{mode}/sum': all_errors_sum,
                        f'mse/{dataset_key}/{mode}/relative': relative_errors_sum}
         return errors_dict
+
+
+def gram_matrix(input):
+    a, b, c, d = input.size()  # a=batch size(=1)
+    # b=number of feature maps
+    # (c,d)=dimensions of a f. map (N=c*d)
+
+    features = input.view(a * b, c * d)  # resise F_XL into \hat F_XL
+
+    G = torch.mm(features, features.t())  # compute the gram product
+
+    # we 'normalize' the values of the gram matrix
+    # by dividing by the number of element in each feature maps.
+    return G.div(a * b * c * d)
+
+
+class StyleLoss(nn.Module):
+
+    def __init__(self):
+        super(StyleLoss, self).__init__()
+
+
+    def forward(self, predicted_feature, target_feature):
+        G_predicted = gram_matrix(predicted_feature)
+        G_target = gram_matrix(target_feature).detach()
+        loss = F.mse_loss(G_predicted, G_target)
+        return loss
