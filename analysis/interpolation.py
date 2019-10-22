e672f022282fa6283638a1b416a151558198463d
Marcello <marcel.buehler@uzh.ch>
2019-10-22 17:52:40 +0200

    visualization code

--- analysis/interpolation.py
+++ analysis/interpolation.py
@@ -1,12 +1,14 @@
 import os
 from datetime import datetime
-
+import subprocess
 import PIL
 
 from data.postprocessor import ImagePostprocessor
 
 from models.pix2pix_model import Pix2PixModel
 from options.test_options import TestOptions
+
+from util.files import create_folder_if_not_exists
 
 import data
 import cv2
@@ -34,7 +36,11 @@ def style_image_to_grid(data):
     return grid
 
 
-def run_interpolation(index_start, index_end, n=5, write=False, show=False):
+def run_interpolation(index_start, index_end, n=5, write_cat=False, show=False, write_single=False, out_folder=None):
+    if write_single:
+        create_folder_if_not_exists(out_folder)
+        print(f"Outputting single png files to {out_folder}")
+
     if np.abs(index_start - index_end) < 100:
         index_end += 200
 
@@ -77,55 +83,54 @@ def run_interpolation(index_start, index_end, n=5, write=False, show=False):
     # mask = ImagePostprocessor.resize(mask, as_tensor=False)[0][0]
 
     data_i = data_mask
-
-
-    original_start = ImagePostprocessor.to_255resized_imagebatch(style_image_to_grid(data_start), as_tensor=False)[0][0].astype(np.uint8)
-    original_end = ImagePostprocessor.to_255resized_imagebatch(style_image_to_grid(data_end), as_tensor=False)[0][0].astype(
-        np.uint8)
-
-    # results.append(original_start)
-    # results.append(np.zeros((mask.shape[0], border_width), dtype=np.uint8))
-    # results.append(mask.astype(np.uint8))
-
-    for alpha in np.linspace(0, 1, n):
+    for i, alpha in enumerate(np.linspace(0, 1, n)):
         data_i['latent_style'] = latent_style_start * (1 - alpha) + latent_style_end * alpha
         # data_i_end['latent_style'] = latent_style_start * (1 - alpha) + latent_style_end * alpha
 
         fake = model.forward(data_i, mode="inference").detach().cpu()
         fake_resized = ImagePostprocessor.to_255resized_imagebatch(fake, as_tensor=False)[0][0].astype(np.uint8)
+
+
         results.append(fake_resized)
+
+        if write_single:
+            path_out = f'{out_folder}/{i:05d}.png'
+            Image.fromarray(fake_resized).save(path_out)
+
 
         # fake = model.forward(data_i_end, mode="inference").detach().cpu()
         # fake_resized = ImagePostprocessor.to_255resized_imagebatch(fake, as_tensor=False)[0][0].astype(np.uint8)
         # results_end.append(fake_resized)
 
-    # results.append(np.zeros(mask.shape[0], 10))
+    if write_cat or show:
+        # results.append(np.zeros(mask.shape[0], 10))
 
-    # results.append(np.zeros((mask.shape[0], border_width), dtype=np.uint8))
-    # results.append(original_end)
-    # data_i['latent_style'] = latent_style_start
-    # for i in [0, 100, 300, 600]:
-    #     data_new = dataset.get_particular(i)
-    #     data_i['label'] = data_new['label']
-    #     fake = model.forward(data_new, mode="inference").detach().cpu()
-    #     fake_resized = ImagePostprocessor.to_255resized_imagebatch(fake, as_tensor=False)[0][0].astype(np.uint8)
-    #     results.append(fake_resized)
-    cat = np.concatenate(results, axis=-1)
-    # cat = np.concatenate([np.concatenate(results_start, axis=-1), np.concatenate(results_end, axis=-1)], axis=-2)
-    # import matplotlib.pyplot as plt
-    # plt.imshow(cat, cmap='gray')
-    # plt.show()
+        # results.append(np.zeros((mask.shape[0], border_width), dtype=np.uint8))
+        # results.append(original_end)
+        # data_i['latent_style'] = latent_style_start
+        # for i in [0, 100, 300, 600]:
+        #     data_new = dataset.get_particular(i)
+        #     data_i['label'] = data_new['label']
+        #     fake = model.forward(data_new, mode="inference").detach().cpu()
+        #     fake_resized = ImagePostprocessor.to_255resized_imagebatch(fake, as_tensor=False)[0][0].astype(np.uint8)
+        #     results.append(fake_resized)
+        cat = np.concatenate(results, axis=-1)
+        # cat = np.concatenate([np.concatenate(results_start, axis=-1), np.concatenate(results_end, axis=-1)], axis=-2)
+        # import matplotlib.pyplot as plt
+        # plt.imshow(cat, cmap='gray')
+        # plt.show()
 
-    image = Image.fromarray(cat)
-    # current_time = datetime.now().strftime("%Y%m%d-%H%M%S")
-    if write:
-        path_out = f'/home/marcel/Documents/paper/interpolation/selected/{index_start}_{index_end}.png'
-        image.save(path_out)
-        print(f"Saved to {path_out}")
-    if show:
-        cv2.imshow("wind", cat)
-        cv2.waitKey(1000)
-    return cat
+        image = Image.fromarray(cat)
+        # current_time = datetime.now().strftime("%Y%m%d-%H%M%S")
+        if write_cat:
+            path_out = f'/home/marcel/Documents/paper/interpolation/selected/{index_start}_{index_end}.png'
+            image.save(path_out)
+            print(f"Saved to {path_out}")
+        if show:
+            cv2.imshow("wind", cat)
+            cv2.waitKey(1000)
+        return cat
+    return path_out
 
 
 def create_random_figure(n=5):
@@ -249,12 +254,42 @@ combos = (
     (2060, 1209),
     (604, 1006),
     (830, 1097),
-    (1097, 2097),
-    (323, 1340),
+    #(1097, 2097),
+    #(323, 1340),
 )
 # stack(combos)
-for combo in combos:
-    interpolation_video(combo[0], combo[1], n=100)
+
+# Create fake images and video for single images
+def video_single(combos, n):
+    for combo in combos:
+        index_start, index_end = combo[0], combo[1]
+        print(f"Running combo {index_start} {index_end}")
+        out_folder = f'/home/marcel/Documents/paper/interpolation/{index_start}_{index_end}'
+        run_interpolation(index_start, index_end, n=n, write_single=True, out_folder=out_folder)
+        subprocess.run(f"ffmpeg -f image2 -r 20 -i {out_folder}/%05d.png -vcodec mpeg4 -y {out_folder}/{index_start}_{index_end}_{n}.mp4", shell=True)
+
+
+def stack_and_video(combos, n):
+    path_tmp = f'/home/marcel/Documents/paper/interpolation/stacked'
+    create_folder_if_not_exists(path_tmp)
+    for i in range(n):
+        images = list()
+        for combo in combos:
+            in_file = f'/home/marcel/Documents/paper/interpolation/{combo[0]}_{combo[1]}/{i:05d}.png'
+            img = Image.open(in_file)
+            images.append(img)
+        stacked = np.concatenate(images, axis=-1)
+        Image.fromarray(stacked).save(os.path.join(path_tmp, f"{i:05d}.png"))
+    subprocess.run(
+            f"ffmpeg -r 16 -i {path_tmp}/%05d.png -vcodec mpeg4 -q:v 2  -y {path_tmp}/stacked_{n}.avi",
+            #f"avconv -s hd720 -r 20 -i {path_tmp}/%05d.png -vcodec mpeg4 -y {path_tmp}/stacked_{n}.mp4",
+            shell=True)
+
+
+n = 150
+video_single(combos, n)
+stack_and_video(combos, n)
+
 # create_random_single(25)
 # run_single(800)
 # for i in range(25):
